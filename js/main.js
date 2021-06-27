a92b66c542ba54444c2884aa9ce3571609f1b2bf
arunkr21 <arunkr0038@gmail.com>
2021-06-27 13:46:25 +0530

    scroll

--- js/main.js
+++ js/main.js
@@ -31,22 +31,70 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== SCROLL REVEAL ANIMATION ====================*/
 const sr = ScrollReveal({
-    distance: '60px',
     reset: true,
 })
 
-sr.reveal(`.home__data-top`,{
+sr.reveal(`.home__data-top,.projects__title`,{
     origin: 'top',
     interval: 100,
-    duration: 1000
+    duration: 1000.,
+    distance: '60px'
 })
-sr.reveal(`.home__data-title`,{
+sr.reveal(`.home__data-title,.projects__description`,{
     origin: 'top',
     interval: 100,
-    duration: 1500
+    duration: 1500,
+    distance: '60px'
 })
 sr.reveal(`.home__data-subtitle`,{
     origin: 'top',
     interval: 100,
-    duration: 2000
+    duration: 2000,
+    distance: '60px'
 })
+
+sr.reveal(`.projects__card`,{
+    origin: 'top',
+    interval: 100,
+    duration: 1000,
+    distance: '0px'
+})
+sr.reveal(`.projects__title`,{
+    origin: 'top',
+    interval: 100,
+    duration: 1000,
+    distance: '0px'
+})
+sr.reveal(`.projects__description`,{
+    origin: 'top',
+    interval: 100,
+    duration: 1500,
+    distance: '0px'
+})
+sr.reveal(`.projects__button`,{
+    origin: 'top',
+    interval: 100,
+    duration: 2000,
+    distance: '0px'
+})
+// sr.reveal(`.projects__title`,{
+//     origin: 'top',
+//     interval: 100,
+//     duration: 2000
+// })
+
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("projects__images");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 4000); // Change image every 2 seconds
+}
